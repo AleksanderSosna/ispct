1e35457ea137449d3ad6abd0ca47e30ab7730ed5
Aleksander Sosna <aleksander.sosnatree@gmail.com>
2025-04-11 11:32:56 +0200

    components break-up

--- src/components/landing-page/LandingPage.tsx
+++ src/components/landing-page/LandingPage.tsx
@@ -1,190 +1,23 @@
 import React, { useEffect, useState } from "react";
-import "./LandingPage.css";
 
-interface Offer {
-    id: string;
-    itemNo: string;
-    name: string;
-    imageUrl: string;
-    urls: { [countryCode: string]: { url: string } };
-    prices: { [countryCode: string]: { currencyCode: string; price: number } };
-    bestDiscount: number;
-    bestValue: number;
-    worstPrice: number;
-    shopName?: string;
-}
+import OffersPage from "../offers-page/OffersPage";
 
 const LandingPage: React.FC = () => {
-    const [offers, setOffers] = useState<Offer[]>([]);
-    const [loading, setLoading] = useState<boolean>(true);
-    const [currentIndex, setCurrentIndex] = useState<number>(0);
-
-    const [shop1Offers, setShop1Offers] = useState<Offer[]>([]);
-    const [shop2Offers, setShop2Offers] = useState<Offer[]>([]);
-    const [shop1Index, setShop1Index] = useState<number>(0);
-    const [shop2Index, setShop2Index] = useState<number>(0);
-
     const [selectedCountry, setSelectedCountry] = useState<string>("pl");
+    const [selectedCurrency, setSelectedCurrency] = useState<string>("PLN");
     const [selectedOffersBy, setSelectedOffersBy] = useState<"byValue" | "byDiscount">("byValue");
 
-    const getCurrencyCode = (country: string): string => {
-        switch (country) {
-            case "pl":
-                return "PLN";
-            case "cz":
-                return "CZK";
-            default:
-                return "PLN";
-        }
+    const handleCountryChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectedCountry(event.target.value);
     };
 
-    const handleCountryChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        const newCountry = event.target.value;
-        setSelectedCountry(newCountry);
-        setLoading(true);
-
-        fetch(`https://stores-api-polyu.ondigitalocean.app/api/offers/best.json`)
-            .then((response) => response.json())
-            .then(
-                (data: {
-                    [countryCode: string]: {
-                        byValue: Offer[];
-                        byDiscount: Offer[];
-                    };
-                }) => {
-                    setOffers(data[newCountry][selectedOffersBy]);
-                    setLoading(false);
-                }
-            )
-            .catch((error) => {
-                console.error("Error fetching offers:", error);
-                setLoading(false);
-            });
-
-        const fetchPromises = [];
-        for (const shop of ["ikea", "ecco"]) {
-            fetchPromises.push(
-                fetch(`https://stores-api-polyu.ondigitalocean.app/api/offers/${shop}.json`)
-                    .then((res) => res.json())
-                    .then(
-                        (data: {
-                            [countryCode: string]: {
-                                byValue: Offer[];
-                                byDiscount: Offer[];
-                            };
-                        }) => {
-                            if (shop === "ikea") setShop1Offers(data[newCountry][selectedOffersBy]);
-                            else if (shop === "ecco") setShop2Offers(data[newCountry][selectedOffersBy]);
-                        }
-                    )
-            );
-        }
-        Promise.all(fetchPromises).catch((error) => console.error("Error fetching shop offers:", error));
+    const handleCurrencyChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectedCurrency(event.target.value);
     };
 
     const handleOffersByChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        const newOffersBy = event.target.value as "byValue" | "byDiscount";
-        setSelectedOffersBy(newOffersBy);
-        setLoading(true);
-
-        fetch(`https://stores-api-polyu.ondigitalocean.app/api/offers/best.json`)
-            .then((response) => response.json())
-            .then(
-                (data: {
-                    [countryCode: string]: {
-                        byValue: Offer[];
-                        byDiscount: Offer[];
-                    };
-                }) => {
-                    setOffers(data[selectedCountry][newOffersBy]);
-                    setLoading(false);
-                }
-            )
-            .catch((error) => {
-                console.error("Error fetching offers:", error);
-                setLoading(false);
-            });
-
-        const fetchPromises = [];
-        for (const shop of ["ikea", "ecco"]) {
-            fetchPromises.push(
-                fetch(`https://stores-api-polyu.ondigitalocean.app/api/offers/${shop}.json`)
-                    .then((res) => res.json())
-                    .then(
-                        (data: {
-                            [countryCode: string]: {
-                                byValue: Offer[];
-                                byDiscount: Offer[];
-                            };
-                        }) => {
-                            if (shop === "ikea") setShop1Offers(data[selectedCountry][newOffersBy]);
-                            else if (shop === "ecco") setShop2Offers(data[selectedCountry][newOffersBy]);
-                        }
-                    )
-            );
-        }
-        Promise.all(fetchPromises).catch((error) => console.error("Error fetching shop offers:", error));
+        setSelectedOffersBy(event.target.value as "byValue" | "byDiscount");
     };
-
-    const handlePrev = () => {
-        setCurrentIndex((prevIndex) => (prevIndex === 0 ? offers.length - 1 : prevIndex - 1));
-    };
-
-    const handleNext = () => {
-        setCurrentIndex((prevIndex) => (prevIndex === offers.length - 1 ? 0 : prevIndex + 1));
-    };
-
-    const handleShopPrev = (setIndex: React.Dispatch<React.SetStateAction<number>>, offers: Offer[]) => {
-        setIndex((prevIndex) => (prevIndex === 0 ? offers.length - 1 : prevIndex - 1));
-    };
-
-    const handleShopNext = (setIndex: React.Dispatch<React.SetStateAction<number>>, offers: Offer[]) => {
-        setIndex((prevIndex) => (prevIndex === offers.length - 1 ? 0 : prevIndex + 1));
-    };
-
-    useEffect(() => {
-        fetch(`https://stores-api-polyu.ondigitalocean.app/api/offers/best.json`)
-            .then((response) => response.json())
-            .then(
-                (data: {
-                    [countryCode: string]: {
-                        byValue: Offer[];
-                        byDiscount: Offer[];
-                    };
-                }) => {
-                    setOffers(data[selectedCountry][selectedOffersBy]);
-                    setLoading(false);
-                }
-            )
-            .catch((error) => {
-                console.error("Error fetching offers:", error);
-                setLoading(false);
-            });
-
-        const fetchPromises = [];
-        for (const shop of ["ikea", "ecco"]) {
-            fetchPromises.push(
-                fetch(`https://stores-api-polyu.ondigitalocean.app/api/offers/${shop}.json`)
-                    .then((res) => res.json())
-                    .then(
-                        (data: {
-                            [countryCode: string]: {
-                                byValue: Offer[];
-                                byDiscount: Offer[];
-                            };
-                        }) => {
-                            if (shop === "ikea") setShop1Offers(data[selectedCountry][selectedOffersBy]);
-                            else if (shop === "ecco") setShop2Offers(data[selectedCountry][selectedOffersBy]);
-                        }
-                    )
-            );
-        }
-        Promise.all(fetchPromises).catch((error) => console.error("Error fetching shop offers:", error));
-    }, [selectedCountry]);
-
-    if (loading) {
-        return <div>Loading...</div>;
-    }
 
     return (
         <div>
@@ -202,7 +35,31 @@ const LandingPage: React.FC = () => {
                     </div>
                     <div className="navbar-end">
                         <div className="navbar-item">
-                            <div className="control has-icons-left">
+                            <div className="control">
+                                <div className="select">
+                                    <select value={selectedCountry} onChange={handleCountryChange}>
+                                        <option value="pl" selected>
+                                            Poland
+                                        </option>
+                                        <option value="cz">Czechia</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="navbar-item">
+                            <div className="control">
+                                <div className="select">
+                                    <select value={selectedCurrency} onChange={handleCurrencyChange}>
+                                        <option value="PLN" selected>
+                                            PLN
+                                        </option>
+                                        <option value="CZK">CZK</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="navbar-item">
+                            <div className="control">
                                 <div className="select">
                                     <select value={selectedOffersBy} onChange={handleOffersByChange}>
                                         <option value="byValue" selected>
@@ -213,111 +70,12 @@ const LandingPage: React.FC = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className="navbar-item">
-                            <div className="control has-icons-left">
-                                <div className="select">
-                                    <select value={selectedCountry} onChange={handleCountryChange}>
-                                        <option value="pl" selected>
-                                            Poland
-                                        </option>
-                                        <option value="cz">Czechia</option>
-                                    </select>
-                                </div>
-                                <span className="icon is-left">
-                                    <i className="fas fa-globe"></i>
-                                </span>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </nav>
-
-            <h1 className="title-main">Best Offers</h1>
-            <div className="carousel">
-                {offers.map((offer, index) => (
-                    <a href={offer.urls[selectedCountry].url}>
-                        <div key={offer.id} className="carousel-item" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
-                            <img src={offer.imageUrl} alt={`Offer ${offer.id}`} className="offer-image" />
-                            <div className="offer-details">
-                                <div className="discount-ribbon">-{(offer.bestDiscount * 100).toFixed(2)}%</div>
-                                {offer.shopName && <div className="shop-ribbon">{offer.shopName}</div>}
-                                <p className="price-tag">
-                                    <span className="original-price">
-                                        {offer.worstPrice.toFixed(2)} {getCurrencyCode(selectedCountry)}
-                                    </span>{" "}
-                                    {offer.prices[selectedCountry].price.toFixed(2)} {getCurrencyCode(selectedCountry)}
-                                </p>
-                            </div>
-                        </div>
-                    </a>
-                ))}
-            </div>
-            <div className="carousel-controls">
-                <button className="prev-button" onClick={() => handlePrev()}>
-                    &#8592;
-                </button>
-                <button className="next-button" onClick={() => handleNext()}>
-                    &#8594;
-                </button>
-            </div>
-            <>
-                <h2 className="title-section">ikea Offers</h2>
-                <div className="carousel">
-                    {shop1Offers.map((offer, index) => (
-                        <a href={offer.urls[selectedCountry].url}>
-                            <div key={offer.id} className="carousel-item" style={{ transform: `translateX(-${shop1Index * 100}%)` }}>
-                                <img src={offer.imageUrl} alt={`ikea Offer ${offer.id}`} className="offer-image" />
-                                <div className="offer-details">
-                                    <div className="discount-ribbon">-{(offer.bestDiscount * 100).toFixed(2)}%</div>
-                                    <p className="price-tag">
-                                        <span className="original-price">
-                                            {offer.worstPrice.toFixed(2)} {getCurrencyCode(selectedCountry)}
-                                        </span>{" "}
-                                        {offer.prices[selectedCountry].price.toFixed(2)} {getCurrencyCode(selectedCountry)}
-                                    </p>
-                                </div>
-                            </div>
-                        </a>
-                    ))}
-                </div>
-                <div className="carousel-controls">
-                    <button className="prev-button" onClick={() => handleShopPrev(setShop1Index, shop1Offers)}>
-                        &#8592;
-                    </button>
-                    <button className="next-button" onClick={() => handleShopNext(setShop1Index, shop1Offers)}>
-                        &#8594;
-                    </button>
-                </div>
-            </>
-            <>
-                <h2 className="title-section">ecco Offers</h2>
-                <div className="carousel">
-                    {shop2Offers.map((offer, index) => (
-                        <a href={offer.urls[selectedCountry].url}>
-                            <div key={offer.id} className="carousel-item" style={{ transform: `translateX(-${shop2Index * 100}%)` }}>
-                                <img src={offer.imageUrl} alt={`ecco Offer ${offer.id}`} className="offer-image" />
-                                <div className="offer-details">
-                                    <div className="discount-ribbon">-{(offer.bestDiscount * 100).toFixed(2)}%</div>
-                                    <p className="price-tag">
-                                        <span className="original-price">
-                                            {offer.worstPrice.toFixed(2)} {getCurrencyCode(selectedCountry)}
-                                        </span>{" "}
-                                        {offer.prices[selectedCountry].price.toFixed(2)} {getCurrencyCode(selectedCountry)}
-                                    </p>
-                                </div>
-                            </div>
-                        </a>
-                    ))}
-                </div>
-                <div className="carousel-controls">
-                    <button className="prev-button" onClick={() => handleShopPrev(setShop2Index, shop2Offers)}>
-                        &#8592;
-                    </button>
-                    <button className="next-button" onClick={() => handleShopNext(setShop2Index, shop2Offers)}>
-                        &#8594;
-                    </button>
-                </div>
-            </>
+            {["best", "ikea", "ecco"].map((shop) => (
+                <OffersPage shop={shop} selectedCountry={selectedCountry} selectedCurrency={selectedCurrency} selectedOffersBy={selectedOffersBy} />
+            ))}
         </div>
     );
 };
---
+++ src/components/offers-page/OffersPage.tsx
@@ -0,0 +1,99 @@
+import React, { useEffect, useState } from "react";
+
+interface Offer {
+    id: string;
+    itemNo: string;
+    name: string;
+    imageUrl: string;
+    urls: { [countryCode: string]: { url: string } };
+    prices: { [countryCode: string]: { [currencyCode: string]: number } };
+    bestDiscount: number;
+    bestValue: { [currencyCode: string]: number };
+    worstPrice: { [currencyCode: string]: number };
+    shopName?: string;
+}
+
+const OffersPage: React.FC<{ shop: string; selectedCountry: string; selectedCurrency: string; selectedOffersBy: string }> = ({
+    shop,
+    selectedCountry,
+    selectedCurrency,
+    selectedOffersBy,
+}) => {
+    const [loading, setLoading] = useState<boolean>(true);
+    const [offers, setOffers] = useState<Offer[]>([]);
+
+    useEffect(() => {
+        fetch(`https://stores-api-polyu.ondigitalocean.app/api/offers/${shop}.json`)
+            .then((response) => response.json())
+            .then((data) => {
+                setOffers(data[selectedCountry][selectedOffersBy]);
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.error("Error fetching offers:", error);
+                setLoading(false);
+            });
+    }, [selectedCountry, selectedOffersBy]);
+
+    if (loading) {
+        return (
+            <>
+                <h1 className="title is-1">{shop.charAt(0).toUpperCase() + shop.slice(1)}</h1>
+                <h2 className="subtitle">Loading...</h2>
+            </>
+        );
+    }
+
+    if (offers.length === 0) {
+        return (
+            <>
+                <h1 className="title is-1">{shop.charAt(0).toUpperCase() + shop.slice(1)}</h1>
+                <h2 className="subtitle">no {shop.charAt(0).toUpperCase() + shop.slice(1)} offers at the time, sorry...</h2>
+            </>
+        );
+    }
+
+    return (
+        <>
+            <h1 className="title is-1">{shop.charAt(0).toUpperCase() + shop.slice(1)}</h1>
+            <div className="grid">
+                {offers.map((offer, index) => (
+                    <div className="cell">
+                        <div className="card">
+                            <a href={offer.urls[selectedCountry].url}>
+                                <header className="card-header">
+                                    <p className="card-header-title is-clipped" style={{ maxWidth: "80%" }}>
+                                        {offer.name}
+                                    </p>
+                                    {offer.shopName && <span className="tag is-link">{offer.shopName.charAt(0).toUpperCase() + offer.shopName.slice(1)}</span>}
+                                </header>
+                                <div className="card-image">
+                                    <figure className="image is-4by3">
+                                        <img src={offer.imageUrl} alt={`offer ${offer.id}`} />
+                                    </figure>
+                                    <div className="is-overlay" style={{ top: "auto", padding: "0.5rem" }}>
+                                        <span className="tag is-danger">-{(offer.bestDiscount * 100).toFixed(2)}%</span>
+                                    </div>
+                                </div>
+                            </a>
+                            <div className="card-content">
+                                <div className="content">
+                                    <span className="tag is-success is-medium">
+                                        {offer.prices[selectedCountry][selectedCurrency].toFixed(2)} {selectedCurrency}
+                                    </span>
+                                    <span className="tag is-info is-light">
+                                        <s>
+                                            {offer.worstPrice[selectedCurrency].toFixed(2)} {selectedCurrency}
+                                        </s>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </>
+    );
+};
+
+export default OffersPage;
